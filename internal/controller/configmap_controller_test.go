7ec49cd96feb64cf529ddd184d1c05dcf03c3844
Zach Stone <zach@giantswarm.io>
2025-12-16 15:23:53 -0800

    Error handling

---
+++ internal/controller/configmap_controller_test.go
@@ -0,0 +1,61 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestIsNotFoundError(t *testing.T) {
+	tests := []struct {
+		name     string
+		err      error
+		expected bool
+	}{
+		{
+			name:     "nil error",
+			err:      nil,
+			expected: false,
+		},
+		{
+			name:     "not found error",
+			err:      errors.New("scorecard data not found for github.com/org/repo"),
+			expected: true,
+		},
+		{
+			name:     "other error",
+			err:      errors.New("API rate limit exceeded"),
+			expected: false,
+		},
+		{
+			name:     "wrapped not found error",
+			err:      errors.New("failed to fetch: scorecard data not found for gitlab.com/group/project"),
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := isNotFoundError(tt.err)
+			if result != tt.expected {
+				t.Errorf("isNotFoundError() = %v, want %v for error: %v", result, tt.expected, tt.err)
+			}
+		})
+	}
+}
+
